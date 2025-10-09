af76735b7ec27372b76a53ed07e047e6ec464375
Hector Javier Espinoza Velasco <javier.me.hector@gmail.com>
2025-10-09 14:04:11 -0500

    clean 1

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'dev.sqlite')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret')
